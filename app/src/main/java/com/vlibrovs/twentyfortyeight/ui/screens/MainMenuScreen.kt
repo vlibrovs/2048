7f8cc84178a14dc6b6ea29583615690ee0dd0a4e
vlibrovs <vlad.librovsky@gmail.com>
2022-09-29 15:16:58 +0300

    MainMenu overlay added

--- app/src/main/java/com/vlibrovs/twentyfortyeight/ui/screens/MainMenuScreen.kt
+++ app/src/main/java/com/vlibrovs/twentyfortyeight/ui/screens/MainMenuScreen.kt
@@ -1,9 +1,13 @@
 package com.vlibrovs.twentyfortyeight.ui.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -17,6 +21,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import com.vlibrovs.twentyfortyeight.R
 import com.vlibrovs.twentyfortyeight.common.getValues
+import com.vlibrovs.twentyfortyeight.data.model.game_result.GameResult
 import com.vlibrovs.twentyfortyeight.data.model.theme.Theme
 import com.vlibrovs.twentyfortyeight.ui.common.composables.Button
 import com.vlibrovs.twentyfortyeight.ui.common.composables.IconButton
@@ -32,89 +37,116 @@ fun MainMenuScreen(
     viewModel: MainViewModel
 ) {
     val values = getValues(rememberWindowInfo().screenWidthInfo)
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(brush = Brush.verticalGradient(theme.backgroundGradient.toList()))
-            .padding(values.mainMenuPadding),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            modifier = Modifier.padding(values.mainMenuTitlePadding),
-            text = "2048",
-            color = theme.textColor,
-            fontSize = values.mainMenuTitleSize,
-            fontFamily = Fonts.Poppins,
-            fontWeight = FontWeight.Bold,
-            style = TextStyle(
-                shadow = Shadow(
-                    Color(0x30000000),
-                    blurRadius = 4f,
-                    offset = Offset(0f, 6f)
-                )
-            )
-        )
+    var overlayVisibility by remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(values.mainMenuHeight),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+                .fillMaxSize()
+                .background(brush = Brush.verticalGradient(theme.backgroundGradient.toList()))
+                .padding(values.mainMenuPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(values.buttonHeight),
-                text = stringResource(id = R.string.continueStr),
-                fontSize = values.buttonTextSize,
-                onClick = {
-                    navController.navigate(Screen.Game.route+"/${viewModel.getCurrentGame() == null}")
-                },
-                theme = theme
+            Text(
+                modifier = Modifier.padding(values.mainMenuTitlePadding),
+                text = "2048",
+                color = theme.textColor,
+                fontSize = values.mainMenuTitleSize,
+                fontFamily = Fonts.Poppins,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(
+                    shadow = Shadow(
+                        Color(0x30000000),
+                        blurRadius = 4f,
+                        offset = Offset(0f, 6f)
+                    )
+                )
             )
-
-            Button(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(values.buttonHeight),
-                text = stringResource(id = R.string.new_game),
-                fontSize = values.buttonTextSize,
-                onClick = {
-                    if (viewModel.getCurrentGame() != null) viewModel.finishCurrentGame()
-
-                    navController.navigate(Screen.Game.route+"/true")
-                },
-                theme = theme
-            )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(values.buttonHeight),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                    .height(values.mainMenuHeight),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                IconButton(
+                Button(
                     modifier = Modifier
-                        .fillMaxWidth(0.48f)
-                        .fillMaxHeight(),
-                    icon = painterResource(id = R.drawable.ic_settings),
-                    contentDescription = stringResource(id = R.string.settings),
-                    iconSize = values.mainMenuIconSize,
-                    onClick = { navController.navigate(Screen.Settings.route) },
+                        .fillMaxWidth()
+                        .height(values.buttonHeight),
+                    text = stringResource(id = R.string.continueStr),
+                    fontSize = values.buttonTextSize,
+                    onClick = {
+                        navController.navigate(Screen.Game.route + "/${viewModel.getCurrentGame() == null}")
+                    },
                     theme = theme
                 )
-                IconButton(
+
+                Button(
                     modifier = Modifier
-                        .fillMaxWidth(0.9f)
-                        .fillMaxHeight(),
-                    icon = painterResource(id = R.drawable.ic_bar_chart),
-                    contentDescription = stringResource(id = R.string.stats),
-                    iconSize = values.mainMenuIconSize,
-                    onClick = { navController.navigate(Screen.Stats.route) },
+                        .fillMaxWidth()
+                        .height(values.buttonHeight),
+                    text = stringResource(id = R.string.new_game),
+                    fontSize = values.buttonTextSize,
+                    onClick = {
+                        val currentGame = viewModel.getCurrentGame()
+                        if (currentGame != null) {
+                            overlayVisibility = true
+                        } else navController.navigate(Screen.Game.route + "/true")
+                    },
                     theme = theme
                 )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(values.buttonHeight),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        modifier = Modifier
+                            .fillMaxWidth(0.48f)
+                            .fillMaxHeight(),
+                        icon = painterResource(id = R.drawable.ic_settings),
+                        contentDescription = stringResource(id = R.string.settings),
+                        iconSize = values.mainMenuIconSize,
+                        onClick = { navController.navigate(Screen.Settings.route) },
+                        theme = theme
+                    )
+                    IconButton(
+                        modifier = Modifier
+                            .fillMaxWidth(0.9f)
+                            .fillMaxHeight(),
+                        icon = painterResource(id = R.drawable.ic_bar_chart),
+                        contentDescription = stringResource(id = R.string.stats),
+                        iconSize = values.mainMenuIconSize,
+                        onClick = { navController.navigate(Screen.Stats.route) },
+                        theme = theme
+                    )
+                }
             }
+        }
+        AnimatedVisibility(
+            visible = overlayVisibility,
+            enter = fadeIn(tween(durationMillis = 500)),
+            exit = fadeOut(tween(durationMillis = 500))
+        ) {
+            Overlay(
+                modifier = Modifier.fillMaxSize(),
+                theme = theme,
+                text = "Your current progress will be lost.\nAre you sure?",
+                onButton1Click = {
+                    overlayVisibility = false
+                    viewModel.finishCurrentGame()
+                    navController.navigate(Screen.Game.route + "/true")
+                },
+                button1Text = "New Game",
+                onButton2Click = {
+                    overlayVisibility = false
+                },
+                button2Text = "Cancel"
+            )
         }
     }
 }
